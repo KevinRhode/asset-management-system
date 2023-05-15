2e346cb579f8eaeb0ef288ca35e41ff419bbedab
Kevin Rhode <kevinrhode@hotmail.com>
2023-05-15 19:45:11 -0400

    add: adding get route for location

--- controllers/api/locationRoutes.js
+++ controllers/api/locationRoutes.js
@@ -1,5 +1,25 @@
 const router = require('express').Router();
-// const {  } = require('../models');
+const { Location } = require('../../models/Location');
 // const withAuth = require('../utils/auth');
+
+router.get('/',async (req,res)=>{
+
+    try {
+        //get all locations
+        const locationsData = await Location.findAll();
+        // Serialize data so the template can read it
+        const locations = locationsData.map((location) => location.get({ plain: true }));
+        // Render to screen
+        res.render('locations',{
+            locations,
+            logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
 
 module.exports = router;
