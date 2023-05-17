a5af95b64e6cee3b8b3057d794613b6f6959325e
KevinRhode <77312186+KevinRhode@users.noreply.github.com>
2023-05-17 17:27:04 -0400

    add: adding work on type routes for comp switch

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const locationRoutes = require('./locationRoutes');
+const typeRoutes = require('./typeRoutes')
 // const homeRoutes = require('./homeRoutes');
 
 // router.use('/', homeRoutes);
 router.use('/location', locationRoutes);
+router.use('/type',typeRoutes);
 
 module.exports = router;
--- controllers/api/typeRoutes.js
+++ controllers/api/typeRoutes.js
@@ -2,18 +2,46 @@ const router = require('express').Router();
 const { Type } = require('../../models');
 // const withAuth = require('../utils/auth');
 
+//get All
+router.get('/',async (req,res)=>{
+
+    try {
+        const typeData = await Type.findAll();
+
+        const types = typeData.map((type) => type.get({ plain: true }));
+    
+        return res.status(200).json(types);
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+   
+
+});
+
 //get one
-router.get('',(req,res)=>{
+router.get('/:id',async (req,res)=>{
+    try {
+        const typeData = await Type.findByPk(req.params.id);
 
+        const types = typeData.map((type) => type.get({ plain: true }));
+    
+        return res.status(200).json(types);
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 });
 
-//get all
-router.get('',(req,res)=>{
+router.post('/',async (req,res)=>{
+    try {
+        const newType = await Type.create({
+          ...req.body,
+          user_id: req.session.user_id,
+        });
     
-});
-
-router.post('',(req,res)=>{
-    
+        res.status(200).json(newType);
+      } catch (err) {
+        res.status(400).json(err);
+      }
 });
 
 router.put('',(req,res)=>{
