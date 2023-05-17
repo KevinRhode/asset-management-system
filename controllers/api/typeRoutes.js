0568c6ee78a8f51b4001adc44bec3e235fda21c3
KevinRhode <77312186+KevinRhode@users.noreply.github.com>
2023-05-17 13:35:46 -0400

    add: adding basic route start for type

---
+++ controllers/api/typeRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { Type } = require('../../models');
+// const withAuth = require('../utils/auth');
+
+//get one
+router.get('',(req,res)=>{
+
+});
+
+//get all
+router.get('',(req,res)=>{
+    
+});
+
+router.post('',(req,res)=>{
+    
+});
+
+router.put('',(req,res)=>{
+    
+});
+
+router.delete('',(req,res)=>{
+    
+});
+
+
+module.exports = router;
