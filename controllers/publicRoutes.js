6c177ef601ed01fba28ccf4160304b9a6d09d765
Kevin Rhode <kevinrhode@hotmail.com>
2023-05-17 21:07:37 -0400

    add: testing home page

---
+++ controllers/publicRoutes.js
@@ -0,0 +1,80 @@
+const router = require('express').Router();
+const { Asset, User, Comment } = require('../models');
+// const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+    //   const assetData = await Asset.findAll(
+    //     {
+    //     include: [
+    //       {
+    //         model:Location,
+    //         attributes:['description']
+    //       },
+    //       {
+    //         model:Type,
+    //         attributes:['description']
+    //       },
+          
+    //       {
+    //         model:Comment,
+    //         // attributes:['user_id','description'],
+    //         include:[{
+    //           model:User,
+    //           attributes:['username']
+    //       }]            
+    //       },          
+    //     ],
+    //   }
+    // );      
+  
+    //   const assets = assetData.map((asset)=> asset.get({ plain: true }));
+        
+      res.render('landing', {
+        // layout:'comment',
+        // assets,
+        // comment,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+router.get('/ssssss', async (req, res) => {
+    try {
+      const assetData = await Asset.findAll(
+        {
+        include: [
+          {
+            model:Location,
+            attributes:['description']
+          },
+          {
+            model:Type,
+            attributes:['description']
+          },
+          
+          {
+            model:Comment,
+            // attributes:['user_id','description'],
+            include:[{
+              model:User,
+              attributes:['username']
+          }]            
+          },          
+        ],
+      }
+    );      
+  
+      const assets = assetData.map((asset)=> asset.get({ plain: true }));
+        
+      res.render('Assets', {
+        // layout:'comment',
+        assets,
+        // comment,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
