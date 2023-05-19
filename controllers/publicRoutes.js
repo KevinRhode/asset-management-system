c6f7226e300af3b764a0b002bb1cf4cef6f3f3db
Kevin Rhode <kevinrhode@hotmail.com>
2023-05-18 20:08:52 -0400

    add: adding files for public routes, public js files, started onto basic dashboard

--- controllers/publicRoutes.js
+++ controllers/publicRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Asset, User, Comment } = require('../models');
+const { Asset, User, Comment,Location,Type } = require('../models');
 // const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -40,6 +40,45 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+router.get('/dashboard',async(req,res)=>{
+  try {
+    const assetData = await Asset.findAll(
+      {
+      include: [
+        {
+          model:Location,
+          attributes:['description']
+        },
+        {
+          model:Type,
+          attributes:['description']
+        },
+        
+        {
+          model:Comment,
+          // attributes:['user_id','description'],
+          include:[{
+            model:User,
+            attributes:['username']
+        }]            
+        },          
+      ],
+    }
+  );      
+
+    const assets = assetData.map((asset)=> asset.get({ plain: true }));
+      // return res.status(200).json(assets);
+    res.render('dashboard', {
+      // layout:'main-comment',
+      assets,
+      // comment,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 router.get('/ssssss', async (req, res) => {
     try {
       const assetData = await Asset.findAll(
@@ -77,6 +116,24 @@ router.get('/ssssss', async (req, res) => {
     } catch (err) {
       res.status(500).json(err);
     }
+  });
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('login');
+  });
+  router.get('/sign-up', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('signup');
   });
 
   module.exports = router;
