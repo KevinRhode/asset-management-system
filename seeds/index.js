23cd8498d78c0b539d62968b5480462157937e90
Kevin Rhode <kevinrhode@hotmail.com>
2023-05-18 21:38:11 -0400

    add: adding data for asset, seed sucessful

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,21 @@
 const sequelize = require('../config/connection');
-const { Asset } = require('../models');
+const { Asset, Type, Location } = require('../models');
 
 const assetData = require('./assetData.json');
+const typeData = require('./typeData.json');
+const locationData = require('./locationData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+  await Type.bulkCreate(typeData, {
+    individualHooks: true,
+    returning: true,
+  }); 
 
+  await Location.bulkCreate(locationData, {
+    individualHooks: true,
+    returning: true,
+  }); 
   await Asset.bulkCreate(assetData, {
     individualHooks: true,
     returning: true,
