437ef39adfae4e3c8176dd789784f843471e4b51
Kevin Rhode <kevinrhode@hotmail.com>
2023-05-22 14:37:19 -0400

    add: change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const helpers = require('./utils/helpers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
