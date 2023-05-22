e9a1746769729bbc3a4c0611c7dd887cc965ab3a
KevinRhode <77312186+KevinRhode@users.noreply.github.com>
2023-05-22 16:49:38 -0400

    add:

--- models/index.js
+++ models/index.js
@@ -1,12 +1,18 @@
 
 
-const Comment = require('./Comment');
+// somethielse
+
+
 const Asset = require('./Asset');
+const Comment = require('./Comment');
+
 const Type = require('./Type');
 const User = require('./user');
 const Location = require('./Location');
 
-Asset.belongsTo(Location);
+Asset.belongsTo(Location,{
+    foreignKey:'location_id'
+});
 Asset.belongsTo(Type);
 Asset.hasMany(Comment);
 
